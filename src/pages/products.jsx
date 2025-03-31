ef953c90ac5be541cc02ca2ed39d66f54be6cdd9
Ajay Singh <ajay@Ajays-MacBook-Air.local>
2025-03-31 16:40:05 +0530

    Updated project files and added new components

---
+++ src/pages/products.jsx
@@ -0,0 +1,121 @@
+import imageProduct1 from "../assets/images/1.png"
+import imageProduct2 from "../assets/images/2.png"
+import imageProduct3 from "../assets/images/3.png"
+import imageProduct4 from "../assets/images/4.png"
+import imageProduct5 from "../assets/images/5.png"
+import imageProduct6 from "../assets/images/6.png"
+import imageProduct7 from "../assets/images/7.png"
+import imageProduct8 from "../assets/images/8.png"
+
+
+
+
+
+
+
+
+
+export const products = [
+    {
+        id: 1,
+        name: 'Hertfoid Upholstered Chair',
+        price: 101,
+        image: imageProduct1,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deleniti consequatur molestiae mollitia harum, perspiciatis in at, tenetur, aliquam possimus quaerat natus quod",
+        slug: 'hertfoid-upholstered-chair',
+    },
+
+
+    {
+        id: 2,
+        name: 'Abingdon Upholstered Chair Swivel',
+        price: 141,
+        image: imageProduct2,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deleniti consequatur molestiae mollitia harum, perspiciatis in at, tenetur, aliquam possimus quaerat natus quod",
+        slug: 'bingdon-pholstered-chair-swivel',
+    },
+  
+
+
+    {
+        id: 3,
+        name: 'Hertfoid Upholstered Chair',
+        price: 101,
+        image: imageProduct3,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deleniti consequatur molestiae mollitia harum, perspiciatis in at, tenetur, aliquam possimus quaerat natus quod",
+        slug: 'hertfoid-upholstered-chair',
+    },
+
+
+
+
+
+
+
+    {
+        id: 4,
+        name: 'JJeses Minimore Modern Style',
+        price: 121,
+        image: imageProduct4,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deleniti consequatur molestiae mollitia harum, perspiciatis in at, tenetur, aliquam possimus quaerat natus quod",
+        slug: 'eses-minimore-modern-style-zakari',
+    },
+    
+
+    {
+        id: 5,
+        name: 'Bolanle Upholstered Armchain',
+        price: 131,
+        image: imageProduct5,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deleniti consequatur molestiae mollitia harum, perspiciatis in at, tenetur, aliquam possimus quaerat natus quod",
+        slug: 'corrigan-studio-tufed-upholstered-widewinback-armchain',
+    },
+ 
+
+
+
+    {
+        id: 6,
+        name: 'Jaqueze Upholstered Armchain',
+        price: 131,
+        image: imageProduct6,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deleniti consequatur molestiae mollitia harum, perspiciatis in at, tenetur, aliquam possimus quaerat natus quod",
+        slug: 'corrigan-studio-273-wide-armchain',
+    },
+
+
+
+
+
+
+
+
+    {
+        id: 7,
+        name: 'Leston Wide Upholstered Fabric',
+        price: 191,
+        image: imageProduct7,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deleniti consequatur molestiae mollitia harum, perspiciatis in at, tenetur, aliquam possimus quaerat natus quod",
+        slug: 'Leston-wide-upholstered-fabric',
+    },
+
+
+
+   
+
+
+    {
+        id: 8,
+        name: 'Stephanyy 27.5 Wide Tufted',
+        price: 191,
+        image: imageProduct8,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint deleniti consequatur molestiae mollitia harum, perspiciatis in at, tenetur, aliquam possimus quaerat natus quod",
+        slug: 'stephanny-275-wide-tufted-armchair',
+    },
+
+
+
+    
+]
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // ✅ Make sure this exists
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
